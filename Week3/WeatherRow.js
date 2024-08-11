3a9c0e92d3d7736d9dfc557404c93cc9bdeee171
Saqib Dar <115392179+SaqibDar112@users.noreply.github.com>
2024-08-11 20:32:18 +0530

    Add files via upload

---
+++ Week3/WeatherRow.js
@@ -0,0 +1,13 @@
+const WeatherRow = () => {
+    return (
+        <tr>
+            <td>01 Jan,2023</td>
+            <td>
+                H: 30&deg;C - L:25&deg;C;
+            </td>
+            <td>Sunny</td>
+        </tr>
+    );
+};
+
+export default WeatherRow;
---
+++ Week3/WeatherSummary.js
@@ -0,0 +1,8 @@
+const WeatherSummary = () => {
+    return (
+        <div>
+            <h1>27&deg;C | Sunny</h1>
+        </div>
+    );
+};
+export default WeatherSummary;
